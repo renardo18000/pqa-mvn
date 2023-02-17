6ebf2fe2bd5b310212c7d74b6935c1832185cb41
Simon LONGUET <simon.longuet@ratp.fr>
2023-02-17 17:28:02 +0100

    Initial commit

---
+++ src/main/kotlin/com/pqa/pqamvn/PqaMvnApplication.kt
@@ -0,0 +1,11 @@
+package com.pqa.pqamvn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PqaMvnApplication
+
+fun main(args: Array<String>) {
+	runApplication<PqaMvnApplication>(*args)
+}
---
+++ src/test/kotlin/com/pqa/pqamvn/PqaMvnApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pqa.pqamvn
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PqaMvnApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
